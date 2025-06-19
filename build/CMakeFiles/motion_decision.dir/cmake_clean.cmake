f1200c0d57714b4b29b8441ae243a18a43663409
virginiaceccatelli <virginia.ceccatelli@mail.mcgill.ca>
2025-06-19 23:12:10 +0200

    untracked changes staged

---
+++ build/CMakeFiles/motion_decision.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/motion_decision.dir/motion_decision.cpp.o"
+  "CMakeFiles/motion_decision.dir/motion_decision.cpp.o.d"
+  "motion_decision"
+  "motion_decision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/motion_decision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
